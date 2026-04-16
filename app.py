56f0af642e61ba008f62f1705551d430778db575
MateoFagundezEstevez <mateofagundez2018@gmail.com>
2026-04-16 12:05:47 -0300

    Update app.py

--- app.py
+++ app.py
@@ -19,7 +19,7 @@ df.columns = df.columns.str.strip()
 col1, col2, col3 = st.columns([1, 4, 1])
 
 with col1:
-    st.image("logo.png", width=100)
+    st.image("logo.png", width=220)
 
 with col2:
     st.markdown("## Ferias Internacionales")
